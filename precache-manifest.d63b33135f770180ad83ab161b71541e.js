1f346ccc25d9c931076d9ccccabcfeae2e58d37c
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-09-08 22:31:52 +0300

    Updates

---
+++ precache-manifest.d63b33135f770180ad83ab161b71541e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048843fdbcc57fce2b22d4d4f1d0211f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "adecccc988c83274cf38",
+    "url": "./static/css/main.e7b8a039.chunk.css"
+  },
+  {
+    "revision": "36689209a43d29ff1e8c",
+    "url": "./static/js/2.d4c7f416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4c7f416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adecccc988c83274cf38",
+    "url": "./static/js/main.73549d16.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
